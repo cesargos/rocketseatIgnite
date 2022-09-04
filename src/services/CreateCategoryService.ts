3ca4ab480e77c8203e6ee5fd1be5ad934c527b7d
Cesar Goulart <devcesargoulart@gmail.com>
2022-09-04 20:47:32 -0300

    aplicando S. L. D. do SOLID

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,15 @@
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from '../repositories/ICategoriesRepository';
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+  execute({ name, description }: ICreateCategoryDTO): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) throw new Error('Category already exists!');
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+export { CreateCategoryService };
